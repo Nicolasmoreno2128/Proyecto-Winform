dd31ccc72a38a37dbad90d13f2ea20067122cd43
jaulingui <jaulingui@hotmail.com>
2025-09-07 11:56:20 -0300

    Se completa Marca y Categoria de Lista de Artículo

--- Proyecto Winform/negocio/ArticuloNegocio.cs
+++ Proyecto Winform/negocio/ArticuloNegocio.cs
@@ -20,7 +20,7 @@ namespace negocio
             {
                 conexion.ConnectionString = "server=localhost\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true";
                 comando.CommandType = System.Data.CommandType.Text;
-                comando.CommandText = "SELECT Id, Codigo, Nombre, Descripcion FROM ARTICULOS";
+                comando.CommandText = "SELECT Codigo, Nombre, A.Descripcion, Precio, M.Descripcion Marca, C.Descripcion Categoria FROM ARTICULOS A, MARCAS M, CATEGORIAS C WHERE A.IdMarca = M.Id AND A.IdCategoria = C.Id";
                 comando.Connection = conexion;
 
                 conexion.Open();
@@ -29,10 +29,15 @@ namespace negocio
                 while (lector.Read())
                 {
                     Articulo aux = new Articulo();
-                    aux.Id = lector.GetInt32(0);
+                    //aux.Id = lector.GetInt32(0);
                     aux.Codigo = (string)lector["Codigo"];
                     aux.Nombre = (string)lector["Nombre"];
                     aux.Descripcion = (string)lector["descripcion"];
+                    aux.Precio = (decimal)lector["precio"];
+                    aux.Marca = new Marca();
+                    aux.Marca.Descripcion = (string)lector["Marca"];
+                    aux.Categoria = new Categoria();
+                    aux.Categoria.Descripcion = (string)lector["Categoria"];
 
                     lista.Add(aux);
                 }

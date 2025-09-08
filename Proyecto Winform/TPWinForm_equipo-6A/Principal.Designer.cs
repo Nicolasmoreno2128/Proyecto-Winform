c3043b819e61371f6af7f93304b11f0f0058e4af
jaulingui <jaulingui@hotmail.com>
2025-09-08 19:06:02 -0300

    Actualiación ventana principal

--- Proyecto Winform/TPWinForm_equipo-6A/Principal.Designer.cs
+++ Proyecto Winform/TPWinForm_equipo-6A/Principal.Designer.cs
@@ -29,16 +29,34 @@
         private void InitializeComponent()
         {
             this.grbListas = new System.Windows.Forms.GroupBox();
-            this.lblListas = new System.Windows.Forms.Label();
-            this.btnListaArticulos = new System.Windows.Forms.Button();
+            this.btnListaMarcas = new System.Windows.Forms.Button();
             this.btnListaCategorias = new System.Windows.Forms.Button();
-            this.btnMarcas = new System.Windows.Forms.Button();
+            this.btnListaArticulos = new System.Windows.Forms.Button();
+            this.lblListas = new System.Windows.Forms.Label();
+            this.lblTituloAgregar = new System.Windows.Forms.Label();
+            this.grbAgregar = new System.Windows.Forms.GroupBox();
+            this.btbAgregarMarca = new System.Windows.Forms.Button();
+            this.btbAgregarCategoria = new System.Windows.Forms.Button();
+            this.btbAgregarArticulo = new System.Windows.Forms.Button();
+            this.lblTituloModificar = new System.Windows.Forms.Label();
+            this.grbModificar = new System.Windows.Forms.GroupBox();
+            this.btbModificarMarca = new System.Windows.Forms.Button();
+            this.btnModificarCategoria = new System.Windows.Forms.Button();
+            this.btbModificararticulo = new System.Windows.Forms.Button();
+            this.lblTituloEliminar = new System.Windows.Forms.Label();
+            this.grbEliminar = new System.Windows.Forms.GroupBox();
+            this.btnEliminarMarca = new System.Windows.Forms.Button();
+            this.btnEliminarCategoria = new System.Windows.Forms.Button();
+            this.btnEliminarArticulo = new System.Windows.Forms.Button();
             this.grbListas.SuspendLayout();
+            this.grbAgregar.SuspendLayout();
+            this.grbModificar.SuspendLayout();
+            this.grbEliminar.SuspendLayout();
             this.SuspendLayout();
             // 
             // grbListas
             // 
-            this.grbListas.Controls.Add(this.btnMarcas);
+            this.grbListas.Controls.Add(this.btnListaMarcas);
             this.grbListas.Controls.Add(this.btnListaCategorias);
             this.grbListas.Controls.Add(this.btnListaArticulos);
             this.grbListas.Location = new System.Drawing.Point(17, 49);
@@ -47,15 +65,23 @@
             this.grbListas.TabIndex = 0;
             this.grbListas.TabStop = false;
             // 
-            // lblListas
+            // btnListaMarcas
             // 
-            this.lblListas.AutoSize = true;
-            this.lblListas.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblListas.Location = new System.Drawing.Point(12, 9);
-            this.lblListas.Name = "lblListas";
-            this.lblListas.Size = new System.Drawing.Size(63, 25);
-            this.lblListas.TabIndex = 1;
-            this.lblListas.Text = "Listas";
+            this.btnListaMarcas.Location = new System.Drawing.Point(168, 19);
+            this.btnListaMarcas.Name = "btnListaMarcas";
+            this.btnListaMarcas.Size = new System.Drawing.Size(75, 23);
+            this.btnListaMarcas.TabIndex = 2;
+            this.btnListaMarcas.Text = "Marcas";
+            this.btnListaMarcas.UseVisualStyleBackColor = true;
+            // 
+            // btnListaCategorias
+            // 
+            this.btnListaCategorias.Location = new System.Drawing.Point(87, 19);
+            this.btnListaCategorias.Name = "btnListaCategorias";
+            this.btnListaCategorias.Size = new System.Drawing.Size(75, 23);
+            this.btnListaCategorias.TabIndex = 1;
+            this.btnListaCategorias.Text = "Categorias";
+            this.btnListaCategorias.UseVisualStyleBackColor = true;
             // 
             // btnListaArticulos
             // 
@@ -67,35 +93,180 @@
             this.btnListaArticulos.UseVisualStyleBackColor = true;
             this.btnListaArticulos.Click += new System.EventHandler(this.btnListaArticulos_Click);
             // 
-            // btnListaCategorias
+            // lblListas
             // 
-            this.btnListaCategorias.Location = new System.Drawing.Point(87, 19);
-            this.btnListaCategorias.Name = "btnListaCategorias";
-            this.btnListaCategorias.Size = new System.Drawing.Size(75, 23);
-            this.btnListaCategorias.TabIndex = 1;
-            this.btnListaCategorias.Text = "Categorias";
-            this.btnListaCategorias.UseVisualStyleBackColor = true;
+            this.lblListas.AutoSize = true;
+            this.lblListas.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblListas.Location = new System.Drawing.Point(12, 9);
+            this.lblListas.Name = "lblListas";
+            this.lblListas.Size = new System.Drawing.Size(63, 25);
+            this.lblListas.TabIndex = 1;
+            this.lblListas.Text = "Listas";
             // 
-            // btnMarcas
+            // lblTituloAgregar
             // 
-            this.btnMarcas.Location = new System.Drawing.Point(168, 19);
-            this.btnMarcas.Name = "btnMarcas";
-            this.btnMarcas.Size = new System.Drawing.Size(75, 23);
-            this.btnMarcas.TabIndex = 2;
-            this.btnMarcas.Text = "Marcas";
-            this.btnMarcas.UseVisualStyleBackColor = true;
+            this.lblTituloAgregar.AutoSize = true;
+            this.lblTituloAgregar.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTituloAgregar.Location = new System.Drawing.Point(12, 125);
+            this.lblTituloAgregar.Name = "lblTituloAgregar";
+            this.lblTituloAgregar.Size = new System.Drawing.Size(85, 25);
+            this.lblTituloAgregar.TabIndex = 3;
+            this.lblTituloAgregar.Text = "Agregar";
+            // 
+            // grbAgregar
+            // 
+            this.grbAgregar.Controls.Add(this.btbAgregarMarca);
+            this.grbAgregar.Controls.Add(this.btbAgregarCategoria);
+            this.grbAgregar.Controls.Add(this.btbAgregarArticulo);
+            this.grbAgregar.Location = new System.Drawing.Point(17, 165);
+            this.grbAgregar.Name = "grbAgregar";
+            this.grbAgregar.Size = new System.Drawing.Size(252, 60);
+            this.grbAgregar.TabIndex = 2;
+            this.grbAgregar.TabStop = false;
+            // 
+            // btbAgregarMarca
+            // 
+            this.btbAgregarMarca.Location = new System.Drawing.Point(168, 19);
+            this.btbAgregarMarca.Name = "btbAgregarMarca";
+            this.btbAgregarMarca.Size = new System.Drawing.Size(75, 23);
+            this.btbAgregarMarca.TabIndex = 2;
+            this.btbAgregarMarca.Text = "Marcas";
+            this.btbAgregarMarca.UseVisualStyleBackColor = true;
+            // 
+            // btbAgregarCategoria
+            // 
+            this.btbAgregarCategoria.Location = new System.Drawing.Point(87, 19);
+            this.btbAgregarCategoria.Name = "btbAgregarCategoria";
+            this.btbAgregarCategoria.Size = new System.Drawing.Size(75, 23);
+            this.btbAgregarCategoria.TabIndex = 1;
+            this.btbAgregarCategoria.Text = "Categorias";
+            this.btbAgregarCategoria.UseVisualStyleBackColor = true;
+            // 
+            // btbAgregarArticulo
+            // 
+            this.btbAgregarArticulo.Location = new System.Drawing.Point(6, 19);
+            this.btbAgregarArticulo.Name = "btbAgregarArticulo";
+            this.btbAgregarArticulo.Size = new System.Drawing.Size(75, 23);
+            this.btbAgregarArticulo.TabIndex = 0;
+            this.btbAgregarArticulo.Text = "Artículos";
+            this.btbAgregarArticulo.UseVisualStyleBackColor = true;
+            // 
+            // lblTituloModificar
+            // 
+            this.lblTituloModificar.AutoSize = true;
+            this.lblTituloModificar.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTituloModificar.Location = new System.Drawing.Point(12, 251);
+            this.lblTituloModificar.Name = "lblTituloModificar";
+            this.lblTituloModificar.Size = new System.Drawing.Size(100, 25);
+            this.lblTituloModificar.TabIndex = 5;
+            this.lblTituloModificar.Text = "Modificar";
+            // 
+            // grbModificar
+            // 
+            this.grbModificar.Controls.Add(this.btbModificarMarca);
+            this.grbModificar.Controls.Add(this.btnModificarCategoria);
+            this.grbModificar.Controls.Add(this.btbModificararticulo);
+            this.grbModificar.Location = new System.Drawing.Point(17, 291);
+            this.grbModificar.Name = "grbModificar";
+            this.grbModificar.Size = new System.Drawing.Size(252, 60);
+            this.grbModificar.TabIndex = 4;
+            this.grbModificar.TabStop = false;
+            // 
+            // btbModificarMarca
+            // 
+            this.btbModificarMarca.Location = new System.Drawing.Point(168, 19);
+            this.btbModificarMarca.Name = "btbModificarMarca";
+            this.btbModificarMarca.Size = new System.Drawing.Size(75, 23);
+            this.btbModificarMarca.TabIndex = 2;
+            this.btbModificarMarca.Text = "Marcas";
+            this.btbModificarMarca.UseVisualStyleBackColor = true;
+            // 
+            // btnModificarCategoria
+            // 
+            this.btnModificarCategoria.Location = new System.Drawing.Point(87, 19);
+            this.btnModificarCategoria.Name = "btnModificarCategoria";
+            this.btnModificarCategoria.Size = new System.Drawing.Size(75, 23);
+            this.btnModificarCategoria.TabIndex = 1;
+            this.btnModificarCategoria.Text = "Categorias";
+            this.btnModificarCategoria.UseVisualStyleBackColor = true;
+            // 
+            // btbModificararticulo
+            // 
+            this.btbModificararticulo.Location = new System.Drawing.Point(6, 19);
+            this.btbModificararticulo.Name = "btbModificararticulo";
+            this.btbModificararticulo.Size = new System.Drawing.Size(75, 23);
+            this.btbModificararticulo.TabIndex = 0;
+            this.btbModificararticulo.Text = "Artículos";
+            this.btbModificararticulo.UseVisualStyleBackColor = true;
+            // 
+            // lblTituloEliminar
+            // 
+            this.lblTituloEliminar.AutoSize = true;
+            this.lblTituloEliminar.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTituloEliminar.Location = new System.Drawing.Point(310, 125);
+            this.lblTituloEliminar.Name = "lblTituloEliminar";
+            this.lblTituloEliminar.Size = new System.Drawing.Size(85, 25);
+            this.lblTituloEliminar.TabIndex = 7;
+            this.lblTituloEliminar.Text = "Eliminar";
+            // 
+            // grbEliminar
+            // 
+            this.grbEliminar.Controls.Add(this.btnEliminarMarca);
+            this.grbEliminar.Controls.Add(this.btnEliminarCategoria);
+            this.grbEliminar.Controls.Add(this.btnEliminarArticulo);
+            this.grbEliminar.Location = new System.Drawing.Point(315, 165);
+            this.grbEliminar.Name = "grbEliminar";
+            this.grbEliminar.Size = new System.Drawing.Size(252, 60);
+            this.grbEliminar.TabIndex = 6;
+            this.grbEliminar.TabStop = false;
+            // 
+            // btnEliminarMarca
+            // 
+            this.btnEliminarMarca.Location = new System.Drawing.Point(168, 19);
+            this.btnEliminarMarca.Name = "btnEliminarMarca";
+            this.btnEliminarMarca.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminarMarca.TabIndex = 2;
+            this.btnEliminarMarca.Text = "Marcas";
+            this.btnEliminarMarca.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarCategoria
+            // 
+            this.btnEliminarCategoria.Location = new System.Drawing.Point(87, 19);
+            this.btnEliminarCategoria.Name = "btnEliminarCategoria";
+            this.btnEliminarCategoria.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminarCategoria.TabIndex = 1;
+            this.btnEliminarCategoria.Text = "Categorias";
+            this.btnEliminarCategoria.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarArticulo
+            // 
+            this.btnEliminarArticulo.Location = new System.Drawing.Point(6, 19);
+            this.btnEliminarArticulo.Name = "btnEliminarArticulo";
+            this.btnEliminarArticulo.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminarArticulo.TabIndex = 0;
+            this.btnEliminarArticulo.Text = "Artículos";
+            this.btnEliminarArticulo.UseVisualStyleBackColor = true;
             // 
             // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTituloEliminar);
+            this.Controls.Add(this.grbEliminar);
+            this.Controls.Add(this.lblTituloModificar);
+            this.Controls.Add(this.grbModificar);
+            this.Controls.Add(this.lblTituloAgregar);
+            this.Controls.Add(this.grbAgregar);
             this.Controls.Add(this.lblListas);
             this.Controls.Add(this.grbListas);
             this.Name = "Principal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Principal";
             this.grbListas.ResumeLayout(false);
+            this.grbAgregar.ResumeLayout(false);
+            this.grbModificar.ResumeLayout(false);
+            this.grbEliminar.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,7 +277,22 @@
         private System.Windows.Forms.GroupBox grbListas;
         private System.Windows.Forms.Button btnListaArticulos;
         private System.Windows.Forms.Label lblListas;
-        private System.Windows.Forms.Button btnMarcas;
+        private System.Windows.Forms.Button btnListaMarcas;
         private System.Windows.Forms.Button btnListaCategorias;
+        private System.Windows.Forms.Label lblTituloAgregar;
+        private System.Windows.Forms.GroupBox grbAgregar;
+        private System.Windows.Forms.Button btbAgregarMarca;
+        private System.Windows.Forms.Button btbAgregarCategoria;
+        private System.Windows.Forms.Button btbAgregarArticulo;
+        private System.Windows.Forms.Label lblTituloModificar;
+        private System.Windows.Forms.GroupBox grbModificar;
+        private System.Windows.Forms.Button btbModificarMarca;
+        private System.Windows.Forms.Button btnModificarCategoria;
+        private System.Windows.Forms.Button btbModificararticulo;
+        private System.Windows.Forms.Label lblTituloEliminar;
+        private System.Windows.Forms.GroupBox grbEliminar;
+        private System.Windows.Forms.Button btnEliminarMarca;
+        private System.Windows.Forms.Button btnEliminarCategoria;
+        private System.Windows.Forms.Button btnEliminarArticulo;
     }
 }

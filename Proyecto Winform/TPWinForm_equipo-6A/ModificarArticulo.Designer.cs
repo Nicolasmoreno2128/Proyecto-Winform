f9f8946fa06b2a3ee41590986d8089126dcc74f9
Nicolasmoreno2128 <nicolas.moreno2128@gmail.com>
2025-09-10 00:14:20 -0300

    Modificar Articulo
    Se crea el botón "Modificar articulo"
    Se desarrolla la primera etapa del funcionamiento del botón.
    Me precarga los datos del dato seleccionado en la lista.

---
+++ Proyecto Winform/TPWinForm_equipo-6A/ModificarArticulo.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace TPWinForm_equipo_6A
+{
+    partial class ModificarArticulo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblModMarca = new System.Windows.Forms.Label();
+            this.lblModCategoria = new System.Windows.Forms.Label();
+            this.cbbMarca = new System.Windows.Forms.ComboBox();
+            this.cbbCategoria = new System.Windows.Forms.ComboBox();
+            this.btnCancelarModArticulo = new System.Windows.Forms.Button();
+            this.btnModArticulo = new System.Windows.Forms.Button();
+            this.lvlModArticulo = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.lblModPrecioArticulo = new System.Windows.Forms.Label();
+            this.lblModDescripcionArticulo = new System.Windows.Forms.Label();
+            this.lblModNombreArticulo = new System.Windows.Forms.Label();
+            this.lblModCodigoArticulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblModMarca
+            // 
+            this.lblModMarca.AutoSize = true;
+            this.lblModMarca.Location = new System.Drawing.Point(69, 302);
+            this.lblModMarca.Name = "lblModMarca";
+            this.lblModMarca.Size = new System.Drawing.Size(37, 13);
+            this.lblModMarca.TabIndex = 31;
+            this.lblModMarca.Text = "Marca";
+            // 
+            // lblModCategoria
+            // 
+            this.lblModCategoria.AutoSize = true;
+            this.lblModCategoria.Location = new System.Drawing.Point(69, 262);
+            this.lblModCategoria.Name = "lblModCategoria";
+            this.lblModCategoria.Size = new System.Drawing.Size(52, 13);
+            this.lblModCategoria.TabIndex = 30;
+            this.lblModCategoria.Text = "Categoria";
+            // 
+            // cbbMarca
+            // 
+            this.cbbMarca.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbbMarca.FormattingEnabled = true;
+            this.cbbMarca.Location = new System.Drawing.Point(70, 318);
+            this.cbbMarca.Name = "cbbMarca";
+            this.cbbMarca.Size = new System.Drawing.Size(121, 21);
+            this.cbbMarca.TabIndex = 29;
+            // 
+            // cbbCategoria
+            // 
+            this.cbbCategoria.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbbCategoria.FormattingEnabled = true;
+            this.cbbCategoria.Location = new System.Drawing.Point(70, 278);
+            this.cbbCategoria.Name = "cbbCategoria";
+            this.cbbCategoria.Size = new System.Drawing.Size(121, 21);
+            this.cbbCategoria.TabIndex = 28;
+            // 
+            // btnCancelarModArticulo
+            // 
+            this.btnCancelarModArticulo.Location = new System.Drawing.Point(144, 356);
+            this.btnCancelarModArticulo.Name = "btnCancelarModArticulo";
+            this.btnCancelarModArticulo.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelarModArticulo.TabIndex = 27;
+            this.btnCancelarModArticulo.Text = "Cancelar";
+            this.btnCancelarModArticulo.UseVisualStyleBackColor = true;
+            // 
+            // btnModArticulo
+            // 
+            this.btnModArticulo.Location = new System.Drawing.Point(51, 356);
+            this.btnModArticulo.Name = "btnModArticulo";
+            this.btnModArticulo.Size = new System.Drawing.Size(75, 23);
+            this.btnModArticulo.TabIndex = 26;
+            this.btnModArticulo.Text = "Modificar";
+            this.btnModArticulo.UseVisualStyleBackColor = true;
+            // 
+            // lvlModArticulo
+            // 
+            this.lvlModArticulo.AutoSize = true;
+            this.lvlModArticulo.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvlModArticulo.Location = new System.Drawing.Point(66, 59);
+            this.lvlModArticulo.Name = "lvlModArticulo";
+            this.lvlModArticulo.Size = new System.Drawing.Size(147, 21);
+            this.lvlModArticulo.TabIndex = 25;
+            this.lvlModArticulo.Text = "Modificar Artículo";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(70, 239);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(121, 20);
+            this.txtPrecio.TabIndex = 24;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(70, 198);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(121, 20);
+            this.txtDescripcion.TabIndex = 23;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(70, 155);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(121, 20);
+            this.txtNombre.TabIndex = 22;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(70, 115);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(121, 20);
+            this.txtCodigo.TabIndex = 21;
+            // 
+            // lblModPrecioArticulo
+            // 
+            this.lblModPrecioArticulo.AutoSize = true;
+            this.lblModPrecioArticulo.Location = new System.Drawing.Point(67, 222);
+            this.lblModPrecioArticulo.Name = "lblModPrecioArticulo";
+            this.lblModPrecioArticulo.Size = new System.Drawing.Size(37, 13);
+            this.lblModPrecioArticulo.TabIndex = 20;
+            this.lblModPrecioArticulo.Text = "Precio";
+            // 
+            // lblModDescripcionArticulo
+            // 
+            this.lblModDescripcionArticulo.AutoSize = true;
+            this.lblModDescripcionArticulo.Location = new System.Drawing.Point(67, 181);
+            this.lblModDescripcionArticulo.Name = "lblModDescripcionArticulo";
+            this.lblModDescripcionArticulo.Size = new System.Drawing.Size(63, 13);
+            this.lblModDescripcionArticulo.TabIndex = 19;
+            this.lblModDescripcionArticulo.Text = "Descripción";
+            // 
+            // lblModNombreArticulo
+            // 
+            this.lblModNombreArticulo.AutoSize = true;
+            this.lblModNombreArticulo.Location = new System.Drawing.Point(67, 138);
+            this.lblModNombreArticulo.Name = "lblModNombreArticulo";
+            this.lblModNombreArticulo.Size = new System.Drawing.Size(44, 13);
+            this.lblModNombreArticulo.TabIndex = 18;
+            this.lblModNombreArticulo.Text = "Nombre";
+            // 
+            // lblModCodigoArticulo
+            // 
+            this.lblModCodigoArticulo.AutoSize = true;
+            this.lblModCodigoArticulo.Location = new System.Drawing.Point(67, 99);
+            this.lblModCodigoArticulo.Name = "lblModCodigoArticulo";
+            this.lblModCodigoArticulo.Size = new System.Drawing.Size(40, 13);
+            this.lblModCodigoArticulo.TabIndex = 17;
+            this.lblModCodigoArticulo.Text = "Código";
+            // 
+            // ModificarArticulo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 450);
+            this.Controls.Add(this.lblModMarca);
+            this.Controls.Add(this.lblModCategoria);
+            this.Controls.Add(this.cbbMarca);
+            this.Controls.Add(this.cbbCategoria);
+            this.Controls.Add(this.btnCancelarModArticulo);
+            this.Controls.Add(this.btnModArticulo);
+            this.Controls.Add(this.lvlModArticulo);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lblModPrecioArticulo);
+            this.Controls.Add(this.lblModDescripcionArticulo);
+            this.Controls.Add(this.lblModNombreArticulo);
+            this.Controls.Add(this.lblModCodigoArticulo);
+            this.Name = "ModificarArticulo";
+            this.Text = "Modificar_Articulo";
+            this.Load += new System.EventHandler(this.ModificarArticulo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblModMarca;
+        private System.Windows.Forms.Label lblModCategoria;
+        private System.Windows.Forms.ComboBox cbbMarca;
+        private System.Windows.Forms.ComboBox cbbCategoria;
+        private System.Windows.Forms.Button btnCancelarModArticulo;
+        private System.Windows.Forms.Button btnModArticulo;
+        private System.Windows.Forms.Label lvlModArticulo;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label lblModPrecioArticulo;
+        private System.Windows.Forms.Label lblModDescripcionArticulo;
+        private System.Windows.Forms.Label lblModNombreArticulo;
+        private System.Windows.Forms.Label lblModCodigoArticulo;
+    }
+}
---
+++ Proyecto Winform/TPWinForm_equipo-6A/ModificarArticulo.cs
@@ -0,0 +1,41 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPWinForm_equipo_6A
+{
+    public partial class ModificarArticulo : Form
+    {
+
+        private Articulo articulo = null;
+        public ModificarArticulo()
+        {
+            InitializeComponent();
+        }
+        public ModificarArticulo(Articulo articulo)
+        {
+            InitializeComponent();
+            this.articulo = articulo;
+        }
+
+        private void ModificarArticulo_Load(object sender, EventArgs e)
+        {
+            //Prueba de carga de datos en los campos
+
+            if (articulo != null)
+            {
+                txtCodigo.Text = articulo.Codigo;
+                txtNombre.Text = articulo.Nombre;
+                txtDescripcion.Text = articulo.Descripcion;
+                txtPrecio.Text = articulo.Precio.ToString();
+            }
+        }
+    }
+}
